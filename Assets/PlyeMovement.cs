3965b3cff3af7ff2199df5fd98aec37a1cbc169e
JoseYepes <44163932+JoseYepes@users.noreply.github.com>
2021-01-05 12:56:51 +0100

    Sumario: Contenido Previo
    ContenidoPrevio

---
+++ Assets/PlyeMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlyeMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(0, 255, 500);
+        //rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime); //Añade 2000de fuerza al eje z
+    }
+}
